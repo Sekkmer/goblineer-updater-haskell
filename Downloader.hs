aa50f8a8d9236f21996423a76d39b77033c46429
Sekkmer <sekkmer@gmail.com>
2018-10-02 20:56:13 +0200

    Update Downloader.hs

--- Downloader.hs
+++ Downloader.hs
@@ -1,61 +1,24 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Downloader where
  
-import Data.Aeson
-import Data.List (sort)
 import Data.Map (Map, lookupGE, insert, delete, empty, toAscList)
+import GHC.Generics (Generic)
 
--- import Data.Text
--- import Control.Applicative
--- import Control.Monad
-import qualified Data.ByteString.Lazy as B
-import Network.HTTP.Conduit (simpleHttp)
-import GHC.Generics
-
-import MarketValue(Item, dataToItem)
+import MarketValue(Items, Item, dataToItem, toItems)
 
 data Bonus = Bonus {
     bonusListId :: Int
 } deriving (Show, Generic)
 
-instance FromJSON Bonus
-instance ToJSON Bonus
-
--- data Modifier = Modifier {
---     type :: Int,
---     value :: Int
--- } deriving (Show, Generic)
-
 data Auction = Auction {
-    -- auc :: Int,
     item :: Int,
-    -- owner :: String,
-    -- ownerRealm :: String,
-    -- bid :: Int,
     buyout :: Int,
     quantity :: Int,
-    -- timeLeft :: String,
-    -- rand :: Int,
-    -- seed :: Int,
-    -- context :: Int,
     bonusLists :: Maybe [Bonus]
-    --modifiers :: Maybe [Modifier]
 } deriving (Show, Generic)
 
-instance FromJSON Auction
-instance ToJSON Auction
-
 data Auctions = Auctions { auctions :: [Auction] } deriving (Show, Generic)
-
-instance FromJSON Auctions
-instance ToJSON Auctions
-
-jsonURL :: String
-jsonURL = "http://auction-api-eu.worldofwarcraft.com/auction-data/ad9b89618cc65dbf04bb7757fd7d2f38/auctions.json"
-
-getJSON :: IO B.ByteString
-getJSON = simpleHttp jsonURL
 
 bonusToInt :: Bonus -> Int
 bonusToInt (Bonus b) = b
@@ -85,9 +48,9 @@ transform :: [Auction] -> Map [Int] [Double] -> Map [Int] [Double]
 transform []     map = map
 transform [x]    map = addAuction map x
 transform (x:xs) map = transform xs (addAuction map x)
-   
+
 calculate :: [ ([Int], [Double]) ] -> [Item]
 calculate list = map dataToItem list
 
-final :: Auctions -> [Item]
-final (Auctions aucts) = calculate (toAscList (transform aucts empty)) 
+final :: Auctions -> Items
+final (Auctions aucts) = toItems (calculate (toAscList (transform aucts empty)))
