bbdaa923f093c4041b367da851a31d08565c6f85
Sekkmer <sekkmer@gmail.com>
2018-10-12 18:25:32 +0200

    Update MarketValue.hs

--- MarketValue.hs
+++ MarketValue.hs
@@ -14,13 +14,11 @@ devide a b = a / (fromIntegral b)
 convertImpl :: [Double] -> Int -> Int -> Double -> ([Double], Double, Int) -> ([Double], Double, Int)
 convertImpl []   _   _   _       (ret, sum, i) = (ret, sum `devide` i, i)
 convertImpl list min max percent (ret, sum, i)
-    | i >= max  = return
-    | i <  min  = recall
-    | ret == [] = recall
-    | otherwise =
-        if y * percent >= x
-            then recall
-            else return
+    | i >= max         = return
+    | i <  min         = recall
+    | ret == []        = recall
+    | y * percent >= x = recall
+    | otherwise        = return
     where
         y      = head ret
         x      = head list
@@ -28,17 +26,14 @@ convertImpl list min max percent (ret, sum, i)
         recall = convertImpl xs min max percent (x : ret, sum + x, (i + 1))
         return = (ret, sum `devide` i, i)
 
-convert :: [Double] -> Int -> Int -> Double -> ([Double], Double, Int)
-convert list min max percent = convertImpl list min max percent ([], 0, 0)
-
 standardDiv :: ([Double], Double, Int) -> Double
 standardDiv (list, avg, count) = sqrt ((sum (map subSquare list)) `devide` (count - 1))
     where subSquare x = (x - avg) * (x - avg)
 
-convertFinal :: [Double] -> Int -> Int -> Double -> Double -> [Double]
-convertFinal list min max percent deviation = filter grater cvdList
+convert :: [Double] -> Int -> Int -> Double -> Double -> [Double]
+convert list min max percent deviation = filter grater cvdList
     where
-        tuple    = convert list min max percent
+        tuple    = convertImpl list min max percent ([], 0, 0) 
         avg      = (\(_, _, x) -> x) tuple
         cvdList  = (\(x, _, _) -> x) tuple
         stdDiv   = standardDiv tuple
@@ -47,8 +42,8 @@ convertFinal list min max percent deviation = filter grater cvdList
 average :: [Double] -> Double
 average list = (sum list) / (fromIntegral (length list))
 
-mValue :: [Double] -> Int -> Double
-mValue list len = average (convertFinal list min max 1.5 1.5)
+convertFinal :: [Double] -> Int -> Double
+convertFinal list len = average (convert list min max 1.5 1.5)
     where
         min   = (len % 15)
         per30 = (len % 30)
@@ -80,6 +75,6 @@ dataToItem (itemUID, list)
         len   = length list
         slist = sort list
         id    = head itemUID
-        mVal  = mValue slist len
+        mVal  = convertFinal slist len
         min   = head slist
         bon   = tail itemUID
